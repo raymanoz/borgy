1c195aa879731c70b9b215b6f50240961d550893
Raymond Barlow <rbarlow@raymanoz.com>
2019-06-26 15:04:00 +1000

    hooked up UI Controls to state

--- src/main/react/src/pages/Home.tsx
+++ src/main/react/src/pages/Home.tsx
@@ -1,7 +1,17 @@
 import React, {Component} from "react";
 import {Scale, Scales} from "./Scale";
 
-export class Home extends Component<{}, { scales: Scales }> {
+export class Home extends Component<{}, { scales: Scales, trialName: String, trialScale: String }> {
+    constructor(props: Readonly<{}>) {
+        super(props);
+        this.state = {
+            scales: [],
+            trialName: "",
+            trialScale: ""
+        };
+        this.renderScaleRadio = this.renderScaleRadio.bind(this);
+    }
+
     componentDidMount() {
         fetch(`http://localhost:9000/scales`)
             .then(result => result.json())
@@ -14,20 +24,28 @@ export class Home extends Component<{}, { scales: Scales }> {
             <span id={"newTrial"}>
                 <fieldset>
                     <legend>Start a new trial</legend>
-                    <label htmlFor="trial-name">Name</label><input id="trial-name" type="text"/><br/>
+                    <label htmlFor="trial-name">Name</label><input id="trial-name" type="text" onChange={event => this.onTrialNameChanged(event.target.value)}/><br/>
                     <label>Scale</label><br/>
-                    {this.state ? this.state.scales.map(Home.renderScaleRadio) : <span>Loading scales</span>}
+                    {this.state ? this.state.scales.map(this.renderScaleRadio) : <span>Loading scales</span>}
                     <button>Begin</button>
                 </fieldset>
             </span>
         </span>
     }
 
-    private static renderScaleRadio(scale: Scale) {
+    private onTrialNameChanged(value: String) {
+        this.setState({trialName: value})
+    }
+
+    private onTrialScaleChanged(value: String) {
+        this.setState({trialScale: value})
+    }
+
+    private renderScaleRadio(scale: Scale) {
         let scaleName = `${scale.name}`;
         let radioId = `scale-${scale.name}`;
-        return <span>
-            <input id={radioId}type="radio" name="scale" value={scaleName}/>
+        return <span key={scaleName}>
+            <input id={radioId} type="radio" name="scale" value={scaleName} onChange={event => this.onTrialScaleChanged(event.target.value)}/>
             <label htmlFor={radioId}>{scaleName}</label><br/>
         </span>;
     }
