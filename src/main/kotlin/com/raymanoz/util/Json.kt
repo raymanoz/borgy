aeb32b1f2a43f225a4d01d2b89f62a03e759123a
Raymond Barlow <rbarlow@raymanoz.com>
2019-11-07 15:14:21 +1000

    removed unused function

--- src/main/kotlin/com/raymanoz/util/Json.kt
+++ src/main/kotlin/com/raymanoz/util/Json.kt
@@ -1,17 +1,8 @@
 package com.raymanoz.util
 
-import org.http4k.format.Jackson
-import org.http4k.format.Jackson.asA
 import java.io.InputStream
 
 object Json {
-    fun <T>parseJsonStream(stream: String): List<T> =
-            Jackson.parse(loadStreamOrDie(stream).use { s ->
-                s.bufferedReader().use { r ->
-                    r.readText()
-                }
-            }).asA<Array<T>>().toList()
-
     fun loadStreamOrDie(name: String): InputStream =
             (Json::class.java).getResourceAsStream(name) ?: throw Error("$name not found")
 }
