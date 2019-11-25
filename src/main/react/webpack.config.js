1c9c27ecfe759edb7316219bc6fda0c777f436c9
Raymond Barlow <rbarlow@raymanoz.com>
2019-11-25 13:05:22 +1000

    getting jar stuff working again

--- src/main/react/webpack.config.js
+++ src/main/react/webpack.config.js
@@ -2,50 +2,55 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    entry: './src/index.tsx',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: ['awesome-typescript-loader','tslint-loader'],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(eot|gif|ico|jpe?g|png|svg|ttf|woff2?)$/i,
-                use: 'file-loader'
-            },
+module.exports = (env, argv) => {
+    const app_server = argv.mode === "development" ? "'http://localhost:9000'" : "''";
+
+    console.log("app_server", app_server);
+
+    return {
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        entry: './src/index.tsx',
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: ['awesome-typescript-loader','tslint-loader'],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+                {
+                    test: /\.(eot|gif|ico|jpe?g|png|svg|ttf|woff2?)$/i,
+                    use: 'file-loader'
+                },
+            ],
+        },
+        output: {
+            filename: 'borgy.js',
+            publicPath: "/",
+            path: path.resolve(__dirname, 'build'),
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                filename: 'index.html',
+                inject: 'body'
+            }),
+            new webpack.DefinePlugin({
+                'process.env.REACT_APP_SERVER': app_server
+            })
         ],
-    },
-    output: {
-        filename: 'borgy.js',
-        publicPath: "/",
-        path: path.resolve(__dirname, 'build'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        }),
-        new webpack.DefinePlugin({
-            'process.env.REACT_APP_SERVER': "'http://localhost:9000'"
-        })
-    ],
-    devServer: {
-        historyApiFallback: true
+        devServer: {
+            historyApiFallback: true
+        }
     }
 };
